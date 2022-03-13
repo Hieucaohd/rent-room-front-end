c0395bfbbd0e509b653ac710228e0c65074258d5
hiepnguyen223 <hiepnguyenn223@gmail.com>
2022-03-13 09:43:02 +0700

    add apollo demo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,11 @@ import type { AppProps } from 'next/app';
 import '../styles/index.scss';
 import { ApolloProvider } from '@apollo/client';
 import client from '../lib/apollo-client';
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <ApolloProvider client={client}>
+        <Header />
         <Component {...pageProps} />
     </ApolloProvider>
 }
