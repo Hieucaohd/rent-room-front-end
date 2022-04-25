5f07738565141b1fef29da196396408f853a7047
20020389 <20020389@vnu.edu.vn>
2022-04-26 00:31:55 +0700

    fix error Apollo Client, fix css not load in vercel, fix conflig

--- next.config.js
+++ next.config.js
@@ -1,19 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+       // Warning: This allows production builds to successfully complete even if
+       // your project has ESLint errors.
+       ignoreDuringBuilds: true,
+    },
     reactStrictMode: true,
     eslint: {
-        ignoreDuringBuilds: true,
+        ignoreDuringBuilds: true
     },
     async rewrites() {
-        return {
-            fallback: [
-                {
-                    source: '/api',
-                    destination: process.env.NEXT_PUBLIC_ENDPOINT,
-                },
-            ],
-        };
+      return {
+        fallback: [
+          {
+            source: '/api',
+            destination: process.env.NEXT_PUBLIC_ENDPOINT,
+          },
+        ],
+      }
     },
-};
-
-module.exports = nextConfig;
+  }
+  
+  module.exports = nextConfig
