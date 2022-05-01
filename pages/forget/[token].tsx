9702c60fdc5a3dc4d5e023414629eff638742a7c
hiepnguyen223 <hiepnguyenn223@gmail.com>
2022-05-01 10:07:39 +0700

    Remove verify page

---
+++ pages/forget/[token].tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export interface IVerifyEmailProps {
+}
+
+export default function VerifyEmail (props: IVerifyEmailProps) {
+  return (
+    <div>
+      verify Email
+    </div>
+  );
+}
