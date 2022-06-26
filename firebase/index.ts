5f321aae4e7f252add1a78ca2010ef461dadeec1
20020389 <20020389@vnu.edu.vn>
2022-06-27 00:16:40 +0700

    fix firebase

--- firebase/index.ts
+++ firebase/index.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyAAzgV2U0kIyHHYA5GlS9tez-QnE36H_Vc',
-    authDomain: 'webproject-88b95.firebaseapp.com',
-    projectId: 'webproject-88b95',
-    storageBucket: 'webproject-88b95.appspot.com',
-    messagingSenderId: '766417987541',
-    appId: '1:766417987541:web:bb33350186585a81dc3f5c',
-    measurementId: 'G-L1FB07LPR3',
+    apiKey: 'AIzaSyDiUF4VCtfJpXnPAjaIxqJ4Wxt2E2M0HrM',
+    authDomain: 'rentroom-8f938.firebaseapp.com',
+    projectId: 'rentroom-8f938',
+    storageBucket: 'rentroom-8f938.appspot.com',
+    messagingSenderId: '46805294171',
+    appId: '1:46805294171:web:e687919b1a42224749b37e',
+    measurementId: 'G-GZCYL1M0X1',
 };
 
 // Initialize Firebase
